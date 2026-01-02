4abe889a13c237916625f00a91974add22f99819
NTThuy26 <23110336@student.hcmute.edu.vn>
2026-01-02 22:39:51 +0700

    ensemble library

--- src/ensemble.py
+++ src/ensemble.py
@@ -3,6 +3,11 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.mixture import GaussianMixture
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from scipy.cluster.hierarchy import linkage, fcluster
+from scipy.spatial.distance import pdist
 
 from sklearn.preprocessing import RobustScaler
 
@@ -13,78 +18,101 @@ from algorithms.hierarchical_member import HierarchicalCentroidScratch
 from algorithms.kmeans_member import kmeansScratch
 from algorithms.silhoutte import silhouette_score
 
-diabetesData = pd.read_csv(r"src\data\elliptical_data.csv")
-# diabetesData = pd.read_csv("processed_diabetes_1000.csv")
-xValues = diabetesData.values
+def ensemble_scratch(xScaled, kRange=range(2, 21)):
+    sil_kq = {
+        'ensemble': [],
+        'gmm': [],
+        'hier': [],
+        'kmeans': []
+    }
 
-robustScaler = RobustScaler()
-xScaled = robustScaler.fit_transform(xValues)
-# xScaled = xValues
+    for k in kRange:
+        gmmMember = GMM_Simple(k=k)
+        gmmMember.fit(xScaled)
+        hierMember = HierarchicalCentroidScratch(k=k)
+        kmMember = kmeansScratch(k=k)
+
+        labelsGmm = gmmMember.predict(xScaled)
+        labelsHier = hierMember.fit_predict(xScaled)
+        labelsKm = kmMember.fit_predict(xScaled)
+
+        s_g = max(silhouette_score(xScaled, labelsGmm), 1e-5)
+        s_h = max(silhouette_score(xScaled, labelsHier), 1e-5)
+        s_k = max(silhouette_score(xScaled, labelsKm), 1e-5)
+
+        sil_kq['gmm'].append(s_g)
+        sil_kq['hier'].append(s_h)
+        sil_kq['kmeans'].append(s_k)
+
+        # Tính trọng số Adaptive
+        total = s_g + s_h + s_k
+        w_g, w_h, w_k = s_g/total, s_h/total, s_k/total
+
+        #mx đồng thuận
+        n = xScaled.shape[0]
+        coMatrix = np.zeros((n, n))
+        coMatrix += w_g * (labelsGmm[:, None] == labelsGmm)
+        coMatrix += w_h * (labelsHier[:, None] == labelsHier)
+        coMatrix += w_k * (labelsKm[:, None] == labelsKm)
+
+        #ensemble mx khoang cách
+        distMatrix = 1 - coMatrix
+        np.fill_diagonal(distMatrix, 0)
+        ensemble_model = AgglomerativeClustering(n_clusters=k, metric='precomputed', linkage='complete')
+        labelsEnsemble = ensemble_model.fit_predict(distMatrix)
+
+        sil_kq['ensemble'].append(silhouette_score(xScaled, labelsEnsemble))
+
+    return sil_kq
+
+def ensemble_lib(xScaled, kRange=range(2, 21)):
+    sil_kq = {'ensemble': [], 'gmm': [], 'hier': [], 'km': []}
+
+    for k in kRange:
+        # skleanr
+        m_gmm = GaussianMixture(n_components=k, random_state=42).fit_predict(xScaled)
+        Z = linkage(xScaled, method='centroid', metric = 'euclidean')
+        m_hier = fcluster(Z, t=k, criterion='maxclust') - 1
+        m_km = KMeans(n_clusters=k, random_state=42, n_init='auto').fit_predict(xScaled)
+
+        s_g = max(silhouette_score(xScaled, m_gmm), 1e-5)
+        s_h = max(silhouette_score(xScaled, m_hier), 1e-5)
+        s_k = max(silhouette_score(xScaled, m_km), 1e-5)
+
+        sil_kq['gmm'].append(s_g)
+        sil_kq['hier'].append(s_h)
+        sil_kq['km'].append(s_k)
+
+        #mx đồng thuận
+        w = np.array([s_g, s_h, s_k]) / (s_g + s_h + s_k)
+        coM = (w[0]*(m_gmm[:,None]==m_gmm) + w[1]*(m_hier[:,None]==m_hier) + w[2]*(m_km[:,None]==m_km))
+
+        labelsEns = AgglomerativeClustering(n_clusters=k, metric='precomputed', linkage='complete').fit_predict(1 - coM)
+        sil_kq['ensemble'].append(silhouette_score(xScaled, labelsEns))
+
+    return sil_kq
+
+def plot_comparison(results, kRange, title="So sánh Silhouette"):
+    plt.figure(figsize=(12, 7))
+    plt.plot(kRange, results['ensemble'], 'o-', label='Weighted Ensemble', color='red', linewidth=3)
+    plt.plot(kRange, results['gmm'], 's--', label='GMM', alpha=0.6)
+    plt.plot(kRange, results['hier'], '^--', label='Hierarchical', alpha=0.6)
+    plt.plot(kRange, results['km'], 'x--', label='Kmeans', alpha=0.6)
+
+    plt.title(title)
+    plt.xlabel("k")
+    plt.ylabel("Silhouette")
+    plt.legend()
+    plt.xticks(range(1, 21))
+    plt.yticks(np.arange(-0.25, 1.25, 0.25))
+    plt.grid(True, linestyle=':', alpha=0.6)
+    plt.show()
 
 
-ensembleScores = []
-gmmScores = []
-hierScores = []
-kmScores = []
-kRange = range(2, 21)
-
-# print("Trọng số")
-for k in kRange:
-    gmmMember = GMM_Simple(k=k)
-    gmmMember.fit(xScaled)
-    hierMember = HierarchicalCentroidScratch(k=k)
-    kmMember = kmeansScratch(k=k)
-
-    #NHãn theo từng model đơn
-    labelsGmm = gmmMember.predict(xScaled)
-    labelsHier = hierMember.fit_predict(xScaled)
-    labelsKm = kmMember.fit_predict(xScaled)
-
-    sil_Gmm = max(silhouette_score(xScaled, labelsGmm), 1e-5)
-    sil_Hier = max(silhouette_score(xScaled, labelsHier), 1e-5)
-    sil_Km = max(silhouette_score(xScaled, labelsKm), 1e-5)
-
-    gmmScores.append(sil_Gmm)
-    hierScores.append(sil_Hier)
-    kmScores.append(sil_Km)
-
-    #weight --> lớn ah đến mx đồng thuận
-    tong_Sil = sil_Gmm + sil_Hier + sil_Km
-    weightGmm = sil_Gmm / tong_Sil
-    weightHier = sil_Hier / tong_Sil
-    weightKm = sil_Km / tong_Sil
-    # print(f"{k:<5} | {weightGmm:<12.4f} | {weightHier:<12.4f} | {weightKm:<12.4f}") #xem wei
-
-    # mx đồng thuật
-    nSamples = xScaled.shape[0]
-    weightedCoMatrix = np.zeros((nSamples, nSamples))
-
-    weightedCoMatrix += weightGmm * (labelsGmm[:, np.newaxis] == labelsGmm)
-    weightedCoMatrix += weightHier* (labelsHier[:, np.newaxis] == labelsHier)
-    weightedCoMatrix += weightKm*(labelsKm[:, np.newaxis] == labelsKm)
-
-    #--> K/c --> phân cụm mx đồng thuận
-    distMatrix = 1 - weightedCoMatrix
-    np.fill_diagonal(distMatrix, 0)
-    finalEnsemble = AgglomerativeClustering(n_clusters=k, metric='precomputed', linkage='complete')
-    labelsEnsemble = finalEnsemble.fit_predict(distMatrix)
-
-    ensembleScores.append(silhouette_score(xScaled, labelsEnsemble))
-
-
-plt.figure(figsize=(14, 8))
-#Ensemble, gmm, kmeans, hie
-plt.plot(kRange, ensembleScores, 'o-', label='Weighted Ensemble', linewidth=3, color='red')
-plt.plot(kRange, gmmScores, 's--', label='GMM', alpha=0.7)
-plt.plot(kRange, hierScores, '^--', label='Hierarchical', alpha=0.7)
-plt.plot(kRange, kmScores, 'x--', label='Kmeans', alpha=0.7)
-
-plt.title("So sánh Silhoutte Ensemble - GMM, Hiera, Kmeans", fontsize=14)
-plt.xlabel("k", fontsize=12)
-plt.ylabel("Silhouette", fontsize=12)
-
-plt.xticks(range(1, 21))
-plt.yticks(np.arange(-0.25, 1.25, 0.25))
-plt.grid(True, linestyle=':', alpha=0.6)
-plt.tight_layout()
-plt.show()
+# diabetesData = pd.read_csv("elliptical_data.csv")
+# xValues = diabetesData.values
+# robustScaler = RobustScaler()
+# xScaled = robustScaler.fit_transform(xValues)
+#
+# results_scratch = ensemble_lib(xScaled, range(2, 21))
+# plot_comparison(results_scratch, range(2, 21), "Kết quả của Scratch")
